1342691ccb47946089441ca16c6e5de2f814c217
Bonnie Wang <bonnie.wang.423@gmail.com>
2023-09-08 14:23:17 +1200

    relocate files

--- blog/home.js
+++
@@ -1,8 +0,0 @@
-function addResponsive() {
-  const topNav = document.getElementById('top-nav')
-  if (topNav.className === 'nav') {
-    topNav.className += ' responsive'
-  } else {
-    topNav.className = 'nav'
-  }
-}
