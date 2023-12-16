1e05daa6ff4bf7d3d5fda60fc7f271c4b0dbcb60
Bonnie Wang <bonnie.wang.423@gmail.com>
2023-12-17 09:06:51 +1300

    add head type animation

--- bonnie-portfolio/app/components/HeadSection.tsx
+++ bonnie-portfolio/app/components/HeadSection.tsx
@@ -1,5 +1,7 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation'
 
 const HeadSection = () => {
   return (
@@ -7,8 +9,24 @@ const HeadSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-txl lg:text-6xl font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-br from-teal-700 via-teal-500 to-pink-500">Hi, I&apos;m </span>
-            Bonnie Wang
+            <span className="text-transparent bg-clip-text bg-gradient-to-br from-teal-700 via-teal-500 to-pink-500">
+              Hi, I&apos;m{' '}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                ' Bonnie Wang',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                'Web Developer',
+                1000,
+                'Full Stack Developer',
+                1000,
+              ]}
+              wrapper="span"
+              speed={60}
+              repeat={Infinity}
+            />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Looking for my next opportunity to make a change. The Coding way.
@@ -18,7 +36,9 @@ const HeadSection = () => {
               Hire Me
             </button>
             <button className="px-1 py-3 w-full sm:w-fit rounded-full  bg-transparent bg-gradient-to-br from-teal-700 via-teal-500 to-pink-500 hover:bg-slate-800 text-white  mt-3">
-              <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5'>Downlocad CV</span>
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-3">
+                Downlocad CV
+              </span>
             </button>
           </div>
         </div>
